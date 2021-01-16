961f02219a617ebc8c507f499f46b2d5161ec121
Liem Nguyen <40646564+daniel-liemng@users.noreply.github.com>
2021-01-16 00:24:43 -0500

    User route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,31 @@
-const abc = (req, res) => {};
+const HttpError = require("../models/HttpErrorModel");
+const User = require("../models/User");
 
-module.exports = abc;
+// @route   GET api/users
+// @desc    Get all users
+// @access  Public
+const getAllUsers = async (req, res) => {
+  res.json();
+};
+
+// @route   POST api/users/signup
+// @desc    Sign up
+// @access  Public
+const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const newUser = { name, email, password };
+
+  res.status(201).json();
+};
+
+// @route   POST api/users/login
+// @desc    Login
+// @access  Public
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  res.json();
+};
+
+module.exports = { getAllUsers, signup, login };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,13 +1,21 @@
-// const userController = require("../controllers/userController");
+const userController = require("../controllers/userController");
 // const { userRegisterValidator } = require("../../validator/userValidator");
 
 const router = require("express").Router();
 
-// @route   POST api/users
-// @desc    Register user
+// @route   GET api/users
+// @desc    Get all users
 // @access  Public
-router.get("/", (req, res) => {
-  res.send("ff");
-});
+router.get("/", userController.getAllUsers);
+
+// @route   POST api/users/signup
+// @desc    Sign up
+// @access  Public
+router.post("/signup", userController.signup);
+
+// @route   POST api/users/login
+// @desc    Login
+// @access  Public
+router.post("/login", userController.login);
 
 module.exports = router;
